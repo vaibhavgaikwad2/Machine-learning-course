8a7c397007fa079383633b0db88256d5115489fe
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-01-04 18:38:29 +0530

    new file created

---
+++ 1)Notes/5-Modules and Packages/custom_package/__init__.py
@@ -0,0 +1,4 @@
+"""__init__.py is special fiel used in pyhton to define
+custom packages and their namespaces.
+Its presence signals to Python that a directory should be treated as a package,
+allowing the inclusion of modules from that directory"""
