f30e4cc9e4554afe0e5511d6fd679f84f35b99d2
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-01-28 10:13:30 +0530

    update

---
+++ 1)Notes/12-Logging in python/Real world example of logging/app.py
@@ -0,0 +1,27 @@
+import logging
+
+## configure logging setting
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.FileHandler("app1.log"),
+        logging.StreamHandler()
+    ]
+)
+
+
+# StreamHAndler --> it is resonsible to put all information inside this app1.log file
+ 
+
+logger=logging.getLogger("ArithmaticApp")
+
+def add(a,b):
+    result=a+b
+    logger.debug(f"Adding {a} + {b}", result)
+    return result
+
+
+add(10,5)
