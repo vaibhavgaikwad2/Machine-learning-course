cf6eba6886c515479d4d680dc729c1f9d20da632
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-01-29 11:20:34 +0530

    update

--- 1)Notes/13-Multithreding/multi_processing.py
+++ 1)Notes/13-Multithreding/multi_processing.py
@@ -1,6 +1,11 @@
+## PRocesses that run in parallel
+### CPU-Bound Tasks-Tasks that are heavy on CPU usage (e.g., mathematical computations, data processing).
+## PArallel execution- Multiple cores of the CPU
+
 import multiprocessing
 
-import time 
+import time
+
 def square_numbers():
     for i in range(5):
         time.sleep(1)
@@ -9,22 +14,20 @@ def square_numbers():
 def cube_numbers():
     for i in range(5):
         time.sleep(1.5)
-        print(f"cube: {i*i*i}")
+        print(f"Cube: {i * i * i}")
 
-# Creating entry point
 if __name__=="__main__":
 
-
-    # Creating two processes
+    ## create 2 processes
     p1=multiprocessing.Process(target=square_numbers)
     p2=multiprocessing.Process(target=cube_numbers)
     t=time.time()
-    # Start the process
+
+    ## start the process
     p1.start()
     p2.start()
 
-    #Wait for the process to complete
-
+    ## Wait for the process to complete
     p1.join()
     p2.join()
 
