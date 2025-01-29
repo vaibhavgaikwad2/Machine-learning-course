7e2805c0ca3819f010ddfe7858a789c06041920a
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-01-29 15:53:45 +0530

    update

---
+++ 1)Notes/13-Multithreding/Real world implementation.py
@@ -0,0 +1,9 @@
+'''
+Real world example: Multiprocessing for cpu bound tasks
+scenario: Factorial calculations
+factorial calculation especially for large numbers requires lots of 
+computational work. 
+multiprocessing can be used to distribute the workload across multiple
+cpu cores, improving performance
+
+'''
