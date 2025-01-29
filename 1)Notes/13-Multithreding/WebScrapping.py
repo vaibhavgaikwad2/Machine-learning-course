c871320b7a73618dda859c7074eb3b177caae161
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-01-29 12:07:50 +0530

    update

---
+++ 1)Notes/13-Multithreding/WebScrapping.py
@@ -0,0 +1,5 @@
+# Web scrapping involves making numerous network request to fetch
+# web pages. These tasks are I/O bound they spend a lot of
+# time waiting for responses from servers. Multithreading can significantly
+# improves the performance by allowing multiple web pages to fetched concurrently
+
