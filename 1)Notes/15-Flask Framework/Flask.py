e36679e269443f2d5430d993c3f5d276b07edb54
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-01-30 16:03:04 +0530

    update

---
+++ 1)Notes/15-Flask Framework/Flask.py
@@ -0,0 +1,16 @@
+# Basic skeleton of flask app
+from flask import Flask
+
+
+# Initialzing the flask 
+app=Flask()  #--> It creates an instance of the flask class, which will be your WSGI (web server Gateway interface) application
+
+@app.route("/")  #--> it is a decorator
+
+def welcome():
+     return "welcome this flask app "
+
+
+if __name__=="__main__":  #--> entry point fo an flask application --> from this execution will start.
+     app.run() #--> used to run the flask app
+
