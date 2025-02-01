af1888d9fb8fad1f8e1c92120629f5f843a575a1
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-02-01 10:19:09 +0530

    update

--- 1)Notes/15-Flask Framework/Integrating html page with flask app/flask.py
+++ 1)Notes/15-Flask Framework/Integrating html page with flask app/flask.py
@@ -1,2 +1,19 @@
 from flask import Flask
 
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<html><h1>Welcome</h1></html>"
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
