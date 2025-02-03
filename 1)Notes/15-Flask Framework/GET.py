f397bfe875edf1e698b34cd1ad13e948e53ebb53
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-02-03 09:23:29 +0530

    update

--- 1)Notes/15-Flask Framework/GET.py
+++ 1)Notes/15-Flask Framework/GET.py
@@ -1,20 +1,38 @@
 # GET --> sending a url and getting a content this is GET method
-from flask import Flask, render_template, request
-
+from flask import Flask,render_template,request
+'''
+ It creates an instance of the Flask class, 
+ which will be your WSGI (Web Server Gateway Interface) application.
+'''
+###WSGI Application
 app=Flask(__name__)
 
-@app.route("/",methods=['GET'])
+@app.route("/")
 def welcome():
-    return "Welcome to the flask app"
+    return "<html><H1>Welcome to the flask course</H1></html>"
 
-app.route("/form",methods=['GET','POST'])
+@app.route("/index",methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/form',methods=['GET','POST'])
 def form():
     if request.method=='POST':
         name=request.form['name']
-        return f"hello {name}"
+        return f'Hello {name}!'
+    return render_template('form.html')
+
+@app.route('/submit',methods=['GET','POST'])
+def submit():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}!'
     return render_template('form.html')
 
 
 if __name__=="__main__":
     app.run(debug=True)
-
