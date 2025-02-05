31e87dc0903a7d19465962e9ac2d0b178bc78966
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-02-05 13:42:46 +0530

    update

--- 1)Notes/15-Flask Framework/API.py
+++ 1)Notes/15-Flask Framework/API.py
@@ -51,7 +51,8 @@ def create_item():
 # PUT: for updating a task 
 @app.route('/items/<int:item_id>',methods=['PUT'])
 def update(item_id):
-    
+    item = next((item for item in items if item["id"]== item_id),None)
+
 
 
 
