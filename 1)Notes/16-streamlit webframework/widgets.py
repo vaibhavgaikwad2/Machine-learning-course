765a432df32dbd4affd225e23cc8f32d49d88389
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-02-06 19:44:03 +0530

    update

--- 1)Notes/16-streamlit webframework/widgets.py
+++ 1)Notes/16-streamlit webframework/widgets.py
@@ -1,6 +1,8 @@
 # It is useful to create beautiful webapp
 
 import streamlit as st
+import pandas as pd
+
 
 st.title("streamlit text input")
 name = st.text_input("Enter your name")
@@ -15,3 +17,12 @@ st.write(f"your age is {age}")
 options = ["Python","Java","C++","Javascript"]
 choice = st.selectbox("Choose your favorite language",options)
 st.write(f"You select {choice}.")
+
+
+upload_file=st.file_uploader("Choose csv file",type="csv")
+
+if upload_file is not None:
+    df=pd.read_csv(upload_file)
+    st.write(df)
+
+    
