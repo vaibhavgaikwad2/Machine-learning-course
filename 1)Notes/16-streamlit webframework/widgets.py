9187ee80e7aff56094c696802e3987b061f442aa
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-02-06 19:37:29 +0530

    update

--- 1)Notes/16-streamlit webframework/widgets.py
+++ 1)Notes/16-streamlit webframework/widgets.py
@@ -3,3 +3,15 @@
 import streamlit as st
 
 st.title("streamlit text input")
+name = st.text_input("Enter your name")
+
+if name:
+    st.write(f"Hello {name}")
+
+age = st.slider("Select your age",0,100,18)
+st.write(f"your age is {age}")
+
+
+options = ["Python","Java","C++","Javascript"]
+choice = st.selectbox("Choose your favorite language",options)
+st.write(f"You select {choice}.")
