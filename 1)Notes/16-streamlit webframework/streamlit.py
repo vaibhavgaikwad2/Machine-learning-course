d162f253ae644be374a6c21808df2e42215da095
vaibhavgaikwad2 <vaibhavgaikwad7007@gmail.com>
2025-02-06 10:15:42 +0530

    update

---
+++ 1)Notes/16-streamlit webframework/streamlit.py
@@ -0,0 +1,13 @@
+# Stramlit is open source app framework for machine learning and data science projects. 
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Title of the application
+st.title("Hello Streamlit")
+
+# Display a simple text
+st.write("this is simmple text")
+
+# creating a dataframe
